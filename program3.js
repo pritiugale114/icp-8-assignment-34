84bd3223a5d64017e8817f2c800ce610807ab07e
pritiugale114 <pritiugale114@gmail.com>
2024-07-23 22:41:42 +0530

    code for saying hello to all elements of array

---
+++ program3.js
@@ -0,0 +1,7 @@
+//Program to say Hello to all elements from any array use key of loop
+
+const friends = ["shirisha" , "sidhhi" , "sayliee" , "raniee" ,"nikiee"];
+
+for(let friend of friends){
+    console.log(`hello ${friend}`);
+}
